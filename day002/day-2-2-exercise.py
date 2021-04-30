3ab6ca57cbf04c021ea4e9975ffcd56e7cb54300
raja <mukalaraja@gmail.com>
2021-04-30 22:27:22 +0530

    calculating wt&ht

---
+++ day002/day-2-2-exercise.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+weight= input("enter your weight in kg: ")
+height= input("enter your height in m: ")
+bmi= int(weight) / float(height) **2
+print(bmi)
+
+bmi_as_int= int(bmi)
+print(bmi_as_int)
