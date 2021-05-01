4b9dfc18804a07b221777a8549f05c8b8e303e55
raja <mukalaraja@gmail.com>
2021-05-01 19:24:45 +0530

    day003

--- day001/day003/day-3-start.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
-
-if height >= 120:
-  print("you can ride rollercoaster.")
-else:
-  print("sorry! you have grow taller to go to ride.")
