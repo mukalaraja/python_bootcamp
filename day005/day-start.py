d29718ed593887594d7eeb4a42c2c7d3015eafab
raja <mukalaraja@gmail.com>
2021-05-04 20:28:39 +0530

    loops,ranges,code blocks

---
+++ day005/day-start.py
@@ -0,0 +1,11 @@
+bikes = ["ktm", "duke", "enfiled"]
+for bike in bikes:
+  print(bike)
+  print(bike + " pie")
+
+    #print(bikes)bikes = ["ktm", "duke", "enfiled"]
+for bike in bikes:
+  print(bike)
+  print(bike + " pie")
+
+    #print(bikes)
