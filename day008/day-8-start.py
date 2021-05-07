24d1e6d08f34dd1002ea46282a78fb199fd7cfd3
raja <mukalaraja@gmail.com>
2021-05-07 21:33:19 +0530

    function parameter

---
+++ day008/day-8-start.py
@@ -0,0 +1,33 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+#def greet():
+#  print("Hello")
+#  print("How do you do?")
+#greet()
+
+
+#def greet_with_name(name):
+#  print(f"Hello {name}")
+#  print(f"How do you do {name}")
+#greet_with_name("Raja")
+
+
+#Functions with more than 1 input
+#def greet_with(name, location):
+#  print(f"Hello {name}")
+ # print(f"what is it in like {location} ")
+
+#greet_with("raja", "vizanagaram")
+
+
+# function with keyword arugument:
+
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"what is it in like {location} ")
+
+greet_with(location="vizanagaram", name= "raja",)
