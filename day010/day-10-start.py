d3dbdd329c8b4970726feffcaf3687ff50f65424
raja <mukalaraja@gmail.com>
2021-05-11 23:30:28 +0530

    functions

---
+++ day010/day-10-start.py
@@ -0,0 +1,23 @@
+#def format_name(f_name, l_name):
+ # print(f_name.title())
+  #print(l_name.title())
+
+#format_name("raja", "RaJa")
+
+
+
+#def format_name(f_name, l_name):
+ # formated_f_name = f_name.title()
+  #formated_l_name = l_name.title()
+  #print(f"{formated_f_name} {formated_l_name}")
+#format_name("raja", "mukala")
+
+
+def format_name(f_name, l_name):
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+
+  return f"{formated_f_name} {formated_l_name}"
+print(format_name("raja", "mukala"))
+
+
