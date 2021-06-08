0ca1637e344e19cfd2a819082b4897e5e16e576d
raja <mukalaraja@gmail.com>
2021-06-08 21:11:26 +0530

    data analysing

--- day025/day-1.py
+++ day025/day-1.py
@@ -15,10 +15,10 @@ while len(guessed_state) < 50:
   answer_state = screen.textinput(title=f"{len(guessed_state)}/50 states correct", prompt= "what's the another state name?").title()
 
   if answer_state == "Exit":
-    missing_states = []
-    for state in all_states:
-      if state not in guessed_state:
-        missing_states.append(state)
+    missing_states = [state for state in all_states if state not in guessed_states]
+    #for state in all_states:
+     # if state not in guessed_state:
+      #  missing_states.append(state)
     new_data = pandas.DataFrame(missing_states)
     new_data.to_csv("states_to_learn.csv")
     #print(missing_states)
