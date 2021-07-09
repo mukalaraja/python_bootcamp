78a3cab055f3f65c71d9b90a4a2b733cd52ea045
raja <mukalaraja@gmail.com>
2021-07-09 21:10:15 +0530

    scraping with beautiful soup

---
+++ day045/main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(url)
+website_html = response.text
+soup = BeautifulSoup(website_html, "html.parser")
+all_movies = soup.find_all(name="h3", class_="title")
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
